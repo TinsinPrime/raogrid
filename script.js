3e1ddac70fe32d3ce4198ff153d024f43540248a
Tinsin <53393330+TinsinPrime@users.noreply.github.com>
2024-05-08 01:24:30 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,32 @@ let rownum = 0;
 let colnum = 4;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 let data = '{ "games" : [' +
 	'{ "title":"The Second Reality Project" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=4983"}' +
 	',{ "title":"The Second Reality Project 2" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=4974"}' +
 	',{ "title":"Super Mario Infinity" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=19547"}' +
+	',{ "title":"Super Challenge World" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=9914"}' +
+	',{ "title":"Mario X World Deluxe" , "type":"smw", "site":"https://www.angelfire.com/pokemon2/pika22/hack/oldwork/oldwork.htm"}' +
+	',{ "title":"Mario X World SER" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=10183"}' +
+	',{ "title":"Mario X World BSB" , "type":"smw", "site":"https://www.angelfire.com/pokemon2/pika22/hack/oldwork/oldwork.htm"}' +
+	',{ "title":"Super Mario World Plus" , "type":"smw", "site":"https://web.archive.org/web/20040607153933/http://www.angelfire.com/games4/smwplus/1.html"}' +
+	',{ "title":"Super Mario World Master Courses" , "type":"smw", "site":"http://smwdb.me/db/6/6f5548898168cea1d9752fc5b9b271a94fcac2fa/"}' +
+	',{ "title":"VIP and Wall Mix" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=9706"}' +
+	',{ "title":"Super Mario Infinity 2" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=20859"}' +
+	',{ "title":"The Reture" , "type":"smw", "site":"http://smwdb.me/db/f/f0e8e4b7137bbe969e9d04971af63765b3c357f2/"}' +
 	',{ "title":"Commander Keen" , "type":"steam", "site":"https://store.steampowered.com/app/9180/Commander_Keen/"}' +
 	',{ "title":"Super Marisa World" , "type":"indie", "site":"https://en.touhouwiki.net/wiki/Super_Marisa_World"}' +
 	',{ "title":"EVW" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=10001"}' +
@@ -17,18 +39,18 @@ let data = '{ "games" : [' +
 	',{ "title":"Nonsense" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=35707"}' +
 	']}'
 
+
+
+
+
+
+
+
+
+
+
+
 const games = JSON.parse(data);
-
-
-
-
-
-
-
-
-
-
-
 
 $(function() {
 	for (let i = 0; i < games.games.length; i++){
