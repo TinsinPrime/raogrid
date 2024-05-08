88eae8c644583cd5441e35a7e9fd3f174ec5aaeb
Tinsin <53393330+TinsinPrime@users.noreply.github.com>
2024-05-07 22:29:22 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ let rownum = 0;
 
 
 let data = '{ "games" : [' +
-	'{ "title":"The Second Reality Project" , "img":"The Second Reality Project", "site":"https://www.smwcentral.net/?p=section&a=details&id=4983"}' +
-	',{ "title":"The Second Reality Project 2" , "img":"The Second Reality Project 2", "site":"https://www.smwcentral.net/?p=section&a=details&id=4974"}' +
-	',{ "title":"A Super Mario Thing" , "img":"A Super Mario Thing", "site":"https://www.smwcentral.net/?p=section&a=details&id=17125"}' +
-	',{ "title":"An SMWC Production" , "img":"An SMWC Production", "site":"https://www.smwcentral.net/?p=section&a=details&id=5420"}' +
-	',{ "title":"Nonsense" , "img":"Nonsense", "site":"https://www.smwcentral.net/?p=section&a=details&id=35707"}' +
+	'{ "title":"The Second Reality Project" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=4983"}' +
+	',{ "title":"The Second Reality Project 2" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=4974"}' +
+	',{ "title":"Super Mario Infinity" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=19547"}' +
+	',{ "title":"Commander Keen" , "type":"steam", "site":"https://store.steampowered.com/app/9180/Commander_Keen/"}' +
+	',{ "title":"Super Marisa World" , "type":"indie", "site":"https://en.touhouwiki.net/wiki/Super_Marisa_World"}' +
+	',{ "title":"EVW" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=10001"}' +
+	',{ "title":"A Super Mario Thing" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=17125"}' +
+	',{ "title":"Kirbys Dream Land 3" , "type":"emu", "site":"https://www.igdb.com/games/kirby-s-dream-land-3"}' +
+	',{ "title":"VVVVVV" , "type":"steam", "site":"https://store.steampowered.com/app/70300/VVVVVV/"}' +
+	',{ "title":"An SMWC Production" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=5420"}' +
+	',{ "title":"Nonsense" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=35707"}' +
 	']}'
 
 const games = JSON.parse(data);
@@ -28,11 +34,12 @@ const games = JSON.parse(data);
 $(function() {
 	for (let i = 0; i < games.games.length; i++){
 		let current = games.games[i]
-		let interior = '<a href="' + current.site + '"><img src="img/' + current.img + '.png" class="img-fluid rounded img-thumbnail" alt="' + current.title + '"></img></a>'
+		let baseimage = '<img src="img/' + current.title + '.png" class="img-fluid rounded img-thumbnail ' + current.type +'-type" alt="' + current.title + '"></img>'
+		let interior = '<a href="' + current.site + '"><div>' + baseimage + '</div></a>'
 		if (i % 3 == 0){
 			$("#content").append('<tr id="row-' + rownum + '">')
 		}
-		$("#row-" + rownum).append('<td><p>' + interior + '</p></td>')
+		$("#row-" + rownum).append('<td>' + interior + '</td>')
 		if (i % 3 == 2){
 			$("#row-" + rownum).append('</tr>')
 			rownum++
