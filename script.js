e4fb9c9f2e8e966ffb2fe4ee5bdc0d6ce352cb4f
Tinsin <53393330+TinsinPrime@users.noreply.github.com>
2024-05-09 08:45:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ let data = '{ "games" : [' +
 	',{ "title":"Mario X World SER" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=10183"}' +
 	',{ "title":"Mario X World BSB" , "type":"smw", "site":"https://www.angelfire.com/pokemon2/pika22/hack/oldwork/oldwork.htm"}' +
 	',{ "title":"Super Mario World Plus" , "type":"smw", "site":"https://web.archive.org/web/20040607153933/http://www.angelfire.com/games4/smwplus/1.html"}' +
-	',{ "title":"Super Mario World Master Courses" , "type":"smw", "site":"http://smwdb.me/db/6/6f5548898168cea1d9752fc5b9b271a94fcac2fa/"}' +
+	',{ "title":"Super Mario World Master Courses" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=9029"}' +
 	',{ "title":"VIP and Wall Mix" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=9706"}' +
 	',{ "title":"Super Mario Infinity 2" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=20859"}' +
 	',{ "title":"The Reture" , "type":"smw", "site":"http://smwdb.me/db/f/f0e8e4b7137bbe969e9d04971af63765b3c357f2/"}' +
@@ -42,7 +42,11 @@ let data = '{ "games" : [' +
 	',{ "title":"The Second Reality Project Reloaded" , "type":"smw", "site":"https://www.smwcentral.net/?a=details&id=16696&p=section"}' +
 	',{ "title":"Misadventures of Plumma Man" , "type":"smw", "site":"http://smwdb.me/db/8/89acdbc536eccd3c5f2c1c80feeb9df816ba2844/"}' +
 	',{ "title":"Super Black Mario" , "type":"smw", "site":"https://web.archive.org/web/20110828065916/http://www7.atpages.jp/smw/view.cgi/1232432088"}' +
+	',{ "title":"Super Black Mario 2" , "type":"smw", "site":"https://web.archive.org/web/20110816162952/http://www7.atpages.jp/smw/view.cgi/1233053286/"}' +
 	',{ "title":"Super Marisa World" , "type":"indie", "site":"https://en.touhouwiki.net/wiki/Super_Marisa_World"}' +
+	',{ "title":"Super Black Mario 3" , "type":"smw", "site":"https://web.archive.org/web/20110828071155/http://www7.atpages.jp/smw/view.cgi/1238155862"}' +
+	',{ "title":"PPP" , "type":"smw", "site":"https://web.archive.org/web/20110826040816/http://www7.atpages.jp/smw/view.cgi/1218383563"}' +
+	',{ "title":"The Mario" , "type":"smw", "site":"https://web.archive.org/web/20160831093139/http://1st.geocities.jp/puriny_ping/MyHack.html"}' +
 	',{ "title":"EVW" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=10001"}' +
 	',{ "title":"A Super Mario Thing" , "type":"smw", "site":"https://www.smwcentral.net/?p=section&a=details&id=17125"}' +
 	',{ "title":"Kirbys Dream Land 3" , "type":"emu", "site":"https://www.igdb.com/games/kirby-s-dream-land-3"}' +
